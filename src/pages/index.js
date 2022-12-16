ee51908c40c91e7948f097733bf19de058049b06
alexissaez <alexis.saez@simplit-solutions.com>
2022-12-16 08:31:50 -0300

    improvement login style

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,6 @@ import { Box, Button, ListItem } from '@mui/material';
 
 export default function Index() {
     return (
-        <NextLink
-            href={'/logout'}
-            passHref
-        >
-            <Button
-                component="a"
-            >
-                LOGOUT
-            </Button>
-        </NextLink>
+        <>Index</>
     )
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,19 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { Button, Box, Container, Typography, TextField, Link } from '@mui/material'
 import { useRouter } from "next/router"
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { useAuth } from "../providers/AuthProvider"
+import Loading from '../components/loading'
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
 
 export default function Login() {
     const router = useRouter()
     const { auth, initializing, getRedirect, clearRedirect, user, error } = useAuth()
+    const [signInInProgress, setSignInInProgress] = useState();
 
     useEffect(() => {
         if (initializing || !user) return;
@@ -21,11 +28,13 @@ export default function Login() {
 
     const onSubmit = async ({ username, password }, { setSubmitting }) => {
         try {
+            setSignInInProgress(true)
             await auth.signIn(username, password)
         }
         catch (e) {
             console.log('error', e)
         }
+        setSignInInProgress(false)
         setSubmitting(false)
     }
 
@@ -34,36 +43,103 @@ export default function Login() {
         password: yup.string().required()
     });
 
+    if (initializing) {
+        return (<Loading text="Application Loading" />)
+    }
+
+    if (signInInProgress) {
+        return (<Loading text="Signing in progress" />)
+    }
+
     return (
-        <Formik
-            initialValues={{ username: '', password: '' }}
-            validationSchema={loginSchema}
-            onSubmit={onSubmit}
-        >
-            {props => (
-                <form onSubmit={props.handleSubmit}>
-                    <input
-                        type="text"
-                        name="username"
-                        onChange={props.handleChange}
-                        onBlur={props.handleBlur}
-                        value={props.values.username}
-                    />
-                    {props.errors.username && props.touched.username && props.errors.username}
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={props.handleChange}
-                        onBlur={props.handleBlur}
-                        value={props.values.password}
-                    />
-                    {props.errors.password && props.touched.password && props.errors.password}
-                    <button type="submit" disabled={props.isSubmitting}>
-                        Submit
-                    </button>
-                </form>
-            )}
-        </Formik>
+        <>
+            <Head>
+                <title>Login | Proyecto Condominio</title>
+            </Head>
+            <Box
+                component="main"
+                sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexGrow: 1,
+                    minHeight: '100%'
+                }}
+            >
+                <Container maxWidth="sm">
+                    <Box sx={{ my: 3 }}>
+                        <Typography
+                            color="textPrimary"
+                            variant="h4"
+                        >
+                            Sign in
+                        </Typography>
+                        <Typography
+                            color="textSecondary"
+                            gutterBottom
+                            variant="body2"
+                        >
+                            Sign in on the internal platform
+                        </Typography>
+                    </Box>
+                    {error ? (
+                        <Stack sx={{ width: '100%' }} spacing={2}>
+                            <Alert severity="error">
+                                <AlertTitle>Login in error:</AlertTitle>
+                                {error.message}
+                            </Alert>
+                        </Stack>
+                    ) : null}
+                    <Formik
+                        initialValues={{ username: '', password: '' }}
+                        validationSchema={loginSchema}
+                        onSubmit={onSubmit}
+                    >
+                        {formik => (
+                            <form onSubmit={formik.handleSubmit}>
+                                <TextField
+                                    error={Boolean(formik.touched.username && formik.errors.username)}
+                                    fullWidth
+                                    helperText={formik.touched.username && formik.errors.username}
+                                    label="Username"
+                                    margin="normal"
+                                    name="username"
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    type="text"
+                                    value={formik.values.username}
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    error={Boolean(formik.touched.password && formik.errors.password)}
+                                    fullWidth
+                                    helperText={formik.touched.password && formik.errors.password}
+                                    label="Password"
+                                    margin="normal"
+                                    name="password"
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    type="password"
+                                    value={formik.values.password}
+                                    variant="outlined"
+                                />
+                                <Box sx={{ py: 2 }}>
+                                    <Button
+                                        color="primary"
+                                        disabled={formik.isSubmitting}
+                                        fullWidth
+                                        size="large"
+                                        type="submit"
+                                        variant="contained"
+                                    >
+                                        Sign In Now
+                                    </Button>
+                                </Box>
+                            </form>
+                        )}
+                    </Formik>
+                </Container>
+            </Box>
+        </>
     )
 }
 
